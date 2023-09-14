75482057ef15d7a589d41fef9650cde5bc41788a
Arnold Siboro <arnold@siboro.com>
2023-09-15 08:41:10 +0900

    Revisions

--- transtoba/popup2.js
+++ transtoba/popup2.js
@@ -258,7 +258,7 @@ async function apply_transtoba() {
 								console.log("apply_transtoba: got out of matching");
 							}
 						}
-						console.log("apply_transtoba: got out of going through all data in dat");
+						console.log("apply_transtoba: got out of going through all data in dat, x="+x);
 						if (!ready) {
 							out += workon.charAt(x);
 							console.log("apply_transtoba: if (!ready), not ready, out=" + out);
